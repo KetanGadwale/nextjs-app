534b7d8531f0963c02b3250f9d5046498dd2c391
KetanGadwale <ketanvgadwale@gmail.com>
2022-08-02 15:12:09 +0530

    feat: index message

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
                         <h2>Documentation &rarr;</h2>
                         <p>
                             Find in-depth information about Next.js features and
-                            API.
+                            API. This is Awesome.
                         </p>
                     </a>
 
